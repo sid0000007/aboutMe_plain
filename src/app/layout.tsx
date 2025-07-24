3d12bc5cc5a19706f9af976d750f6e10823ffb50
sid0000007 <mr.siddharth9267963125@gmail.com>
2025-07-24 15:57:29 +0530

    sdfa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/ghost.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
